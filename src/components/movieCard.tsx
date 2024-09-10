9408069fe5a7b00d5764364870aac67de8e54977
Kayo <Kayohenriquesilva19@gmail.com>
2024-09-10 14:28:06 -0300

    feat: movie cart

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -11,19 +11,22 @@ export function MovieCard({
   title,
   vote_average,
 }: Movie) {
+  const truncateText = (text: string, maxLength: number) => {
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+  };
   return (
     <div className="">
       <div className="flex flex-col items-center justify-center">
         <img
-          className="h-64 w-full rounded-lg object-cover"
+          className="h-72 w-full object-cover"
           src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
           alt={title}
         />
-        <div className="mt-4">
+        <div className="mt-4 text-left">
           <h2 className="text-xl font-semibold">{title}</h2>
-          <p className="text-gray-500">{release_date}</p>
-          <p className="text-gray-500">{overview}</p>
-          <p className="text-gray-500">{vote_average}</p>
+          {/* <p className="text-gray-500">{release_date}</p> */}
+          <p className="text-base text-cyan-900">{vote_average}</p>
+          <p className="mt-4 text-neutral-700">{truncateText(overview, 100)}</p>
         </div>
       </div>
     </div>
--- src/components/movieList.tsx
+++ src/components/movieList.tsx
@@ -23,10 +23,8 @@ export function MovieList() {
           Find out what's trending in the world of cinema
         </p>
       </div>
-      <section>
-        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)}
-        </div>
+      <section className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 lg:grid-cols-4">
+        {movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)}
       </section>
     </div>
   );
